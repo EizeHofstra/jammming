42dc76f7e3b8c8b13067f71cf38e320593eb7890
EizeHofstra <eizehofstra@gmail.com>
2018-03-08 17:27:13 +0100

    New code changes

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,29 +22,27 @@ const Spotify = {
   },
 
   search(term) {
+    const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-      headers: {Authorization: `Bearer${this.acessToken}`}
+      headers: {Authorization: `Bearer${accessToken}`}
     })
     .then(response => response.json())
     .then(jsonResponse => {
-      if (jsonResponse.tracks) {
-        return jsonResponse.tracks.items.map(track => {
-          return {
-            id: track.id,
-            name: track.name,
-            artist: track.artist[0].name,
-            album: track.album.name,
-            uri: track.uri
-          }
-        })
-      }
-      else {
+      if (!jsonResponse.tracks) {
         return [];
       }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
     });
   },
 
   savePlaylist(playlistName, UriArray) {
+    const accessToken = Spotify.getAccessToken();
     if (!playlistName || !UriArray) {
       return;
     }
